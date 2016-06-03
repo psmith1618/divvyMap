3d736cee8b69e8c46eb3a3d285f853c779d36ac7
Phill Smith <psmith1618@gmail.com>
2016-06-03 00:05:43 -0500

    added angular-google-maps

--- client/controllers/stations.js
+++ client/controllers/stations.js
@@ -1,8 +1,9 @@
 // Define the `divvyMapApp` module
-var divvyMapApp = angular.module('divvyMapApp', []);
+var divvyMapApp = angular.module('divvyMapApp', ['uiGmapgoogle-maps']);
 
 // Define the `DivvyMapAppController` controller on the `divvyMapApp` module
 divvyMapApp.controller('divvyMapController', function DivvyMapAppController($scope, $http) {
+  	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
   	$http({
 			method : 'GET',
 			url : 'http://localhost:8080/stations'
